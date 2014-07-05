d8d4da475f6ce1a647ff7165c475c5f04648fc07
Roger Steneteg <roger@steneteg.org>
2014-07-05 18:28:08 +0200

    Add GPLv3 license information

--- that-password.py
+++ that-password.py
@@ -1,3 +1,19 @@
+# That-Password, a tool for distributing passwords
+# Copyright (C) 2014  Roger Steneteg <roger@steneteg.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask, g, url_for, request, redirect, render_template, flash, abort
 from pymongo import MongoClient
